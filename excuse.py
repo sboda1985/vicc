cf7c757aed949304332921d1ff9137bebce085e7
sboda1985 <bodaszilard@gmail.com>
2019-11-17 21:22:35 +0200

    Create excuse.py

---
+++ excuse.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+import random
+
+a = random.randint(0, 10)
+
+print a
+print 'Sajnálom, de nem tudok menni, mert:'.decode('utf-8')
+if a == 0:
+    print 'családi program lesz'
+elif a == 1:
+    print 'nincs tiszta nadrágom'.decode('utf-8')
+elif a == 2:
+    print 'fáj a térdem, s csak fociznom szabad'.decode('utf-8')
+elif a == 3:
+    print 'a sógorom disznót vág'.decode('utf-8')
+elif a == 4:
+    print 'konferencián leszek'.decode('utf-8')
+elif a == 5:
+    print 'az évfolyamtársnőimmel leszek'.decode('utf-8')
+elif a == 6:
+    print 'turnézni megyek'.decode('utf-8')
+elif a == 7:
+    print 'matekórám lesz'.decode('utf-8')
+elif a == 8:
+    print 'jetlagem lesz'.decode('utf-8')
+elif a == 9:
+    print 'a munkatársaimmal leszek'.decode('utf-8')
+elif a == 10:
+    print 'képzésem lesz'.decode('utf-8')
+
