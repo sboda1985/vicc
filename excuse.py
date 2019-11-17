a23191da9d202be4f8ac2bf5003a545bc250ef33
sboda1985 <bodaszilard@gmail.com>
2019-11-17 21:23:10 +0200

    Update excuse.py

--- excuse.py
+++ excuse.py
@@ -4,27 +4,27 @@ import random
 a = random.randint(0, 10)
 
 print a
-print 'Sajnálom, de nem tudok menni, mert:'.decode('utf-8')
+print 'Sajnálom, de nem tudok menni, mert:'
 if a == 0:
     print 'családi program lesz'
 elif a == 1:
-    print 'nincs tiszta nadrágom'.decode('utf-8')
+    print 'nincs tiszta nadrágom'
 elif a == 2:
-    print 'fáj a térdem, s csak fociznom szabad'.decode('utf-8')
+    print 'fáj a térdem, s csak fociznom szabad'
 elif a == 3:
-    print 'a sógorom disznót vág'.decode('utf-8')
+    print 'a sógorom disznót vág'
 elif a == 4:
-    print 'konferencián leszek'.decode('utf-8')
+    print 'konferencián leszek'
 elif a == 5:
-    print 'az évfolyamtársnőimmel leszek'.decode('utf-8')
+    print 'az évfolyamtársnőimmel leszek'
 elif a == 6:
-    print 'turnézni megyek'.decode('utf-8')
+    print 'turnézni megyek'
 elif a == 7:
-    print 'matekórám lesz'.decode('utf-8')
+    print 'matekórám lesz'
 elif a == 8:
-    print 'jetlagem lesz'.decode('utf-8')
+    print 'jetlagem lesz'
 elif a == 9:
-    print 'a munkatársaimmal leszek'.decode('utf-8')
+    print 'a munkatársaimmal leszek'
 elif a == 10:
-    print 'képzésem lesz'.decode('utf-8')
+    print 'képzésem lesz'
 
